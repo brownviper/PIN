a9b61bf503fc9dbd76bdc447dd222d5fa83a3777
Saz <s4saz@hotmail.com>
2015-06-01 20:33:21 +0100

    added PIN sol and PinGenTests

---
+++ PIN-TESTS/PinGenTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace PIN_TESTS
+{
+    [TestFixture]
+    public class PinGenTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail("Yay, I m running and failling too");
+        }
+    }
+}
