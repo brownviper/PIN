a48a9fe0aa7337712abff4fffb50d116f67ad41c
Saz <s4saz@hotmail.com>
2015-06-01 21:42:27 +0100

    4 digit random number is returned

--- PIN-GEN/PinGen.cs
+++ PIN-GEN/PinGen.cs
@@ -1,10 +1,20 @@
-﻿namespace PIN_GEN
+﻿using System;
+using System.Security.Cryptography;
+
+namespace PIN_GEN
 {
     public class PinGen
     {
+        private Random random;
+
+        public PinGen()
+        {
+            random = new Random();
+        }
+
         public int Generate()
         {
-            return 1234;
+            return random.Next(1000, 9999);
         }
     }
 }
--- PIN-TESTS/PinGenTests.cs
+++ PIN-TESTS/PinGenTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
 using PIN_GEN;
 
 namespace PIN_TESTS
@@ -10,7 +12,24 @@ namespace PIN_TESTS
         public void Generate_should_retur_4_digit_number()
         {
             var pinGen = new PinGen();
-            Assert.That(pinGen.Generate(), Is.EqualTo(1234));
+            var pin = pinGen.Generate();
+
+            string pattern = @"\d\d\d\d";
+            var regex = new Regex(pattern);
+            var matches = regex.Matches(pin.ToString(CultureInfo.InvariantCulture));
+
+            Assert.That(matches.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Generate_should_return_random_4_digit_pin_number()
+        {
+            var pinGen = new PinGen();
+
+            var first_pin = pinGen.Generate();
+            var second_pin = pinGen.Generate();
+
+            Assert.That(first_pin, Is.Not.EqualTo(second_pin));
         }
     }
 }
