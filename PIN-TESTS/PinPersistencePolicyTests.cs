fd1831ee159c4e3b7995b74aac8c7729c607822b
Saz <s4saz@hotmail.com>
2015-06-02 20:55:18 +0100

    a slight refactor
    Signed-off-by: Saz <s4saz@hotmail.com>

--- PIN-TESTS/PinPersistencePolicyTests.cs
+++ PIN-TESTS/PinPersistencePolicyTests.cs
@@ -10,11 +10,16 @@ namespace PIN_TESTS
     {
         private Mock<FileSystemFacade> file_system;
         private PinPersistence pin_persistence;
+        private int[] stored_pins;
 
         [SetUp]
         public void SetUp()
         {
+            stored_pins = new[] { 1234, 5678, 9101 };
+            
             file_system = new Mock<FileSystemFacade>();
+            file_system.Setup(o => o.LoadPins(It.IsAny<String>())).Returns(stored_pins);
+
             pin_persistence = new PinPersistence(file_system.Object);
         }
 
@@ -22,10 +27,6 @@ namespace PIN_TESTS
         [Test]
         public void HasBeenGenerated_should_return_true_if_pin_is_stored()
         {
-            var stored_pins = new[] {1234, 5678, 9101};
-
-            file_system.Setup(o => o.LoadPins(It.IsAny<String>())).Returns(stored_pins);
-
             Assert.That(pin_persistence.HasBeenGenerated(5678), Is.True);
             file_system.Verify(o => o.LoadPins(It.IsAny<String>()), Times.Once);
         }
@@ -33,10 +34,6 @@ namespace PIN_TESTS
         [Test]
         public void HasBeenGenerated_should_return_falas_if_pin_is_not_stored()
         {
-            var stored_pins = new[] { 1234, 5678, 9101 };
-
-            file_system.Setup(o => o.LoadPins(It.IsAny<String>())).Returns(stored_pins);
-
             Assert.That(pin_persistence.HasBeenGenerated(7575), Is.False);
             file_system.Verify(o => o.LoadPins(It.IsAny<String>()), Times.Once);
         }
@@ -44,12 +41,16 @@ namespace PIN_TESTS
         [Test]
         public void HasBeenGenerated_should_return_false_if_no_pin_is_stored()
         {
-            var stored_pins = new int[]{};
-
-            file_system.Setup(o => o.LoadPins(It.IsAny<String>())).Returns(stored_pins);
+            file_system.Setup(o => o.LoadPins(It.IsAny<String>())).Returns(new int[]{});
 
             Assert.That(pin_persistence.HasBeenGenerated(5678), Is.False);
             file_system.Verify(o => o.LoadPins(It.IsAny<String>()), Times.Once);
+        }
+
+        [Test]
+        public void When_the_pin_is_not_stored_it_should_as_the_file_system_to_store_it()
+        {
+            
         }
     }
 }
